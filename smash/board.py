79faa29c59bc36e4275e57cdde18d03ee4395cd0
maurizio <maurizio.sambati@gmail.com>
2013-02-09 19:14:00 +0100

    add method to set board check status

--- smash/board.py
+++ smash/board.py
@@ -1,4 +1,8 @@
-from smash.base import BaseBoard, SquareHelper, START_POSITION
+import numpy as np
+
+from smash.base import BaseBoard, SquareHelper, START_POSITION, rank, col, pair2square
+from movegen import gen_knight_moves, gen_bishop_moves, gen_rook_moves
+
 
 # workarounds for pyflakes
 SquareHelper
@@ -7,4 +11,37 @@ START_POSITION
 
 class Board(BaseBoard):
     def set_check_status(self):
+        king = {'w': 'K', 'b': 'k'}[self.stm]
+        sq = np.where(self.raw == king)[0][0]
+
+        other_knight = {'w': 'n', 'b': 'N'}[self.stm]
+        for m in gen_knight_moves(self, sq):
+            if m.capture == other_knight:
+                return True
+
+        other = {'w': 'bq', 'b': 'BQ'}[self.stm]
+        for m in gen_bishop_moves(self, sq):
+            if m.capture in other:
+                return True
+
+        other = {'w': 'rq', 'b': 'RQ'}[self.stm]
+        for m in gen_rook_moves(self, sq):
+            if m.capture in other:
+                return True
+
+        r = rank(sq)
+        c = col(sq)
+
+        if self.stm == 'w':
+            r += 1
+        else:
+            r -= 1
+
+        other = {'w': 'p', 'b': 'P'}
+        if 0 <= r <= 7:
+            if c > 0 and self.raw[pair2square(r, c - 1)] == other:
+                return True
+            elif c < 7 and self.raw[pair2square(r, c + 1)] == other:
+                return True
+
         return False
